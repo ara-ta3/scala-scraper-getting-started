603e95e998b1789beb490fc402fdf84d64e48dad
Arata Tanaka <tarata43@yahoo.co.jp>
2016-05-15 15:52:14 +0900

    [ADD] Sources

---
+++ build.sbt
@@ -0,0 +1,7 @@
+scalaVersion := "2.11.8"
+
+name := "scala-scraping-getting-started"
+
+libraryDependencies ++= Seq(
+  "net.ruippeixotog" %% "scala-scraper" % "1.0.0"
+)
---
+++ src/main/scala/com/ru/waka/Main.scala
@@ -0,0 +1,56 @@
+package com.ru.waka
+
+import net.ruippeixotog.scalascraper.browser._
+import net.ruippeixotog.scalascraper.dsl.DSL._
+import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
+import net.ruippeixotog.scalascraper.dsl.DSL.Parse._
+
+object Main {
+  val dummy = """
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+        <title>dummy</title>
+      </head>
+      <body>
+        <h1>h1tag</h1>
+        <div class="contents">
+          <ul>
+            <li>a</li>
+            <li>b</li>
+            <li>c</li>
+          </ul>
+        </div>
+        <div id="aaa">
+        </div>
+      </body>
+      <script>
+        var span  = document.createElement("span");
+        span.innerText= "aaa";
+        document.getElementById("aaa").appendChild(span)
+      </script>
+    </html>
+  """
+  val jsoupBrowser = JsoupBrowser()
+  val htmlUnitBrowser = HtmlUnitBrowser()
+  def main(args: Array[String]): Unit = {
+    val doc = jsoupBrowser.parseString(dummy)
+    println(doc >> text("title"))
+    println(doc >> text("h1"))
+    println(doc >> elementList("li"))
+    println(doc >> attr("charset")("meta"))
+    println(doc >?> attr("charset")("title"))
+    println(doc >> text("#aaa"))
+
+
+    val doc2 = htmlUnitBrowser.parseString(dummy)
+    println(doc2 >> text("title"))
+    println(doc2 >> text("h1"))
+    println(doc2 >> elementList("li"))
+    println(doc2 >> attr("charset")("meta"))
+    println(doc2 >?> attr("charset")("title"))
+    println(doc2 >> text("#aaa"))
+
+  }
+}
